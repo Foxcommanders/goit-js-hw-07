b5b0b6fae462f0aeb533b70dc0978fc5b6babf0a
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2023-06-07 14:02:25 +0300

    Up 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const cardList = document.querySelector('.gallery');
+const galleryList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  })
+  .join('');
+
+cardList.insertAdjacentHTML('afterbegin', galleryList);
+cardList.addEventListener('click', handleImgClick);
+
+function handleImgClick(e) {
+  e.preventDefault();
+  if (e.target === e.currentTarget) {
+    return;
+  }
+  const currentList = e.target.closest('.gallery__image');
+  const sourceList = currentList.dataset.source;
+  const currentDescr = currentList.alt;
+
+  const instance = basicLightbox.create(`
+    <img
+    src="${sourceList}"
+    alt="${currentDescr}"
+  />
+`);
+  instance.show();
+  document.addEventListener('keydown', e =>
+    e.key === 'Escape' ? instance.close() : null
+  );
+}
